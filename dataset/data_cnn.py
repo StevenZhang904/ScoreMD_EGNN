7e4192d8eda668f4d6a920df5dabb9a2f5720cb0
StevenZhang904 <2437949628@qq.com>
2023-05-26 15:57:00 -0400

    starting at test.ipynb to help to gather data for circular 22

--- dataset/data_cnn.py
+++ dataset/data_cnn.py
@@ -56,6 +56,45 @@ class CNN_Dataset(Dataset):
 
 
 
+class Diffusion_Demo_Dataset(Dataset):
+    def __init__(self, sys_data_dir: str, mem_data_dir: str, transform = None):
+        super(Diffusion_Demo_Dataset, self).__init__()
+        self.sys_data_dir = sys_data_dir
+        self.mem_data_dir = mem_data_dir ### In images (png) format
+        self.transform = transform
+
+        self.imgs_dir, labels = [], []
+        sys_data = pd.read_csv(self.sys_data_dir)
+        displacements = torch.tensor(sys_data[1].values)
+        # water_counts = torch.tensor(sys_data["water_counts"].values) 
+        sys_file_names = sys_data["sys_filename"].values
+        # mem_img_list = os.listdir(mem_data_dir)
+
+        for x in range(len(sys_file_names)):
+            sys_file_name = sys_file_names[x]
+            y = "circular_22"
+            if sys_file_name == y:
+                # img_dir = mem_data_dir+ y
+                # self.imgs_dir.append(img_dir)
+                labels.append(displacements[x])
+
+        self.labels = np.array(labels, dtype = np.float32)
+        self.len = len(self.labels)
+
+    def __len__(self):
+        return self.len
+    
+    def __getitem__(self, index):
+        img = Image.open(self.imgs_dir[index])
+        ## Raw images has four channels, converted to RGB to become 3 channels
+        img = img.convert("RGB")
+        img = self.transform(img)
+        img = np.array(img) / 255.0
+        img = np.moveaxis(img, 2, 0)
+        img = torch.from_numpy(img).type(torch.FloatTensor)
+        label = self.labels[index]
+        return img , label
+
 
 
 
