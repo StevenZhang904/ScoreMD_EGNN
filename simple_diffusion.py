de73f56995b05bacc6d104ebe9ff1e414fc6c34f
StevenZhang904 <2437949628@qq.com>
2023-05-30 17:15:18 -0400

    updated simple_diffusion

--- simple_diffusion.py
+++ simple_diffusion.py
@@ -8,21 +8,164 @@ import torchvision
 from torchvision import transforms
 from torch.optim import Adam, lr_scheduler
 from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
 from tqdm import tqdm
-from dataset.data_cnn import CNN_Dataset
+from dataset.data_diffusion_demo import Diffusion_Dataset
 import time 
 import math
 
-def cosine_beta_schedule(timesteps, s=0.008, **kwargs):
+def extract(a, t, x_shape):
     """
-    cosine schedule as proposed in https://arxiv.org/abs/2102.09672
+    从给定的张量a中检索特定的元素。t是一个包含要检索的索引的张量，
+    这些索引对应于a张量中的元素。这个函数的输出是一个张量，
+    包含了t张量中每个索引对应的a张量中的元素
+    :param a:
+    :param t:
+    :param x_shape:
+    :return:
     """
-    steps = timesteps + 1
-    x = torch.linspace(0, timesteps, steps)
-    alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * math.pi * 0.5) ** 2
-    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
-    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
-    return torch.clip(betas, 0.0001, 0.9999)
+    batch_size = t.shape[0]
+    out = a.gather(-1, t.cpu())
+    return out.reshape(batch_size, *((1,) * (len(x_shape) - 1))).to(t.device)
+
+
+class DiffusionModel(nn.Module):
+    def __init__(self,timesteps=1000,denoise_model=None):
+        super(DiffusionModel, self).__init__()
+
+
+        self.denoise_model = denoise_model
+
+        # 方差生成
+        self.timesteps = timesteps
+        self.betas = self.cosine_beta_schedule(timesteps, s = 0.008)
+        # define alphas
+        self.alphas = 1. - self.betas
+        self.alphas_cumprod = torch.cumprod(self.alphas, dim=0)
+        self.alphas_cumprod_prev = F.pad(self.alphas_cumprod[:-1], (1, 0), value=1.0)
+        self.sqrt_recip_alphas = torch.sqrt(1.0 / self.alphas)
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        # x_t  = sqrt(alphas_cumprod)*x_0 + sqrt(1 - alphas_cumprod)*z_t
+        self.sqrt_alphas_cumprod = torch.sqrt(self.alphas_cumprod)
+        self.sqrt_one_minus_alphas_cumprod = torch.sqrt(1. - self.alphas_cumprod)
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        # 这里用的不是简化后的方差而是算出来的
+        self.posterior_variance = self.betas * (1. - self.alphas_cumprod_prev) / (1. - self.alphas_cumprod)
+
+    def q_sample(self, x_start, t, noise=None):
+        # forward diffusion (using the nice property)
+        # x_t  = sqrt(alphas_cumprod)*x_0 + sqrt(1 - alphas_cumprod)*z_t
+        if noise is None:
+            noise = torch.randn_like(x_start)
+
+        sqrt_alphas_cumprod_t = extract(self.sqrt_alphas_cumprod, t, x_start.shape)
+        sqrt_one_minus_alphas_cumprod_t = extract(
+            self.sqrt_one_minus_alphas_cumprod, t, x_start.shape
+        )
+
+        return sqrt_alphas_cumprod_t * x_start + sqrt_one_minus_alphas_cumprod_t * noise
+
+    def compute_loss(self, x_start, t, noise=None, loss_type="l2"):
+        if noise is None:
+            noise = torch.randn_like(x_start)
+
+        x_noisy = self.q_sample(x_start=x_start, t=t, noise=noise)
+        predicted_noise = self.denoise_model(x_noisy, t)
+
+        if loss_type == 'l1':
+            loss = F.l1_loss(noise, predicted_noise)
+        elif loss_type == 'l2':
+            loss = F.mse_loss(noise, predicted_noise)
+        elif loss_type == "huber":
+            loss = F.smooth_l1_loss(noise, predicted_noise)
+        else:
+            raise NotImplementedError()
+        return loss
+
+    @torch.no_grad()
+    def p_sample(self, x, t, t_index):
+        betas_t = extract(self.betas, t, x.shape)
+        sqrt_one_minus_alphas_cumprod_t = extract(
+            self.sqrt_one_minus_alphas_cumprod, t, x.shape
+        )
+        sqrt_recip_alphas_t = extract(self.sqrt_recip_alphas, t, x.shape)
+
+        # Equation 11 in the paper
+        # Use our model (noise predictor) to predict the mean
+        model_mean = sqrt_recip_alphas_t * (
+                x - betas_t * self.denoise_model(x, t) / sqrt_one_minus_alphas_cumprod_t
+        )
+
+        if t_index == 0:
+            return model_mean
+        else:
+            posterior_variance_t = extract(self.posterior_variance, t, x.shape)
+            noise = torch.randn_like(x)
+            # Algorithm 2 line 4:
+            return model_mean + torch.sqrt(posterior_variance_t) * noise
+
+    @torch.no_grad()
+    def p_sample_loop(self, shape):
+        device = next(self.denoise_model.parameters()).device
+
+        b = shape[0]
+        # start from pure noise (for each example in the batch)
+        img = torch.randn(shape, device=device)
+        imgs = []
+
+        for i in tqdm(reversed(range(0, self.timesteps)), desc='sampling loop time step', total=self.timesteps):
+            img = self.p_sample(img, torch.full((b,), i, device=device, dtype=torch.long), i)
+            imgs.append(img.cpu().numpy())
+        return imgs
+
+    @torch.no_grad()
+    def sample(self, image_size, batch_size=16, channels=3):
+        return self.p_sample_loop(shape=(batch_size, channels, image_size, image_size))
+
+    def forward(self, mode, **kwargs):
+        if mode == "train":
+            # 先判断必须参数
+            if "x_start" and "t" in kwargs.keys():
+                # 接下来判断一些非必选参数
+                if "loss_type" and "noise" in kwargs.keys():
+                    return self.compute_loss(x_start=kwargs["x_start"], t=kwargs["t"],
+                                             noise=kwargs["noise"], loss_type=kwargs["loss_type"])
+                elif "loss_type" in kwargs.keys():
+                    return self.compute_loss(x_start=kwargs["x_start"], t=kwargs["t"], loss_type=kwargs["loss_type"])
+                elif "noise" in kwargs.keys():
+                    return self.compute_loss(x_start=kwargs["x_start"], t=kwargs["t"], noise=kwargs["noise"])
+                else:
+                    return self.compute_loss(x_start=kwargs["x_start"], t=kwargs["t"])
+
+            else:
+                raise ValueError("扩散模型在训练时必须传入参数x_start和t！")
+
+        elif mode == "generate":
+            if "image_size" and "batch_size" and "channels" in kwargs.keys():
+                return self.sample(image_size=kwargs["image_size"],
+                                   batch_size=kwargs["batch_size"],
+                                   channels=kwargs["channels"])
+            else:
+                raise ValueError("扩散模型在生成图片时必须传入image_size, batch_size, channels等三个参数")
+        else:
+            raise ValueError("mode参数必须从{train}和{generate}两种模式中选择")
+
+    def cosine_beta_schedule(timesteps, s=0.008):
+        """
+        cosine schedule as proposed in https://arxiv.org/abs/2102.09672
+        """
+        steps = timesteps + 1
+        x = torch.linspace(0, timesteps, steps)
+        alphas_cumprod = torch.cos(((x / timesteps) + s) / (1 + s) * math.pi * 0.5) ** 2
+        alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+        betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+        return torch.clip(betas, 0.0001, 0.9999)
+
+
+
+
 
 class MLP(nn.Module):
     def __init__(self, in_size, zDim = 1):
